211fb0ae8ca13726c6936a7a26269bda11e3e317
filesmuggler <krzste09@outlook.com>
2018-11-16 14:31:03 +0100

    rename changes

---
+++ Zajecia_1/Swiatla_drogowe/Swiatla_drogowe.ino
@@ -0,0 +1,13 @@
+/*
+  MIT License
+  ---
+  Arduino Workshops
+  Class #1
+  Program purpose: Traffic lights simulator
+  ---
+  @author: Krzysztof Stezala
+  ---
+  Provided by CybAiR Science Club at 
+  Institute of Control, Robotics and Information Engineering of
+  Poznan University of Technology  
+*/
