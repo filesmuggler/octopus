46c9a1226ab8335e36ec4d9fc2c99a3ff256d16a
filesmuggler <krzste09@outlook.com>
2019-01-16 00:22:23 +0100

    czujnik

---
+++ Zajecia_5/czujnik/czujnik.ino
@@ -0,0 +1,91 @@
+/*
+  MIT License
+  ---
+  Arduino Workshops
+  Class #5
+  Program purpose: HC-SR04 sensor
+  ---
+  @author: Krzysztof Stezala
+  ---
+  Provided by CybAiR Science Club at 
+  Institute of Control, Robotics and Information Engineering of
+  Poznan University of Technology  
+*/
+
+#define BAUDRATE 9600
+
+#define TRIG_PIN 10
+#define ECHO_PIN 11
+#define BUZZER_PIN 2
+
+int ledPins[] = { 3, 4, 5, 6, 7 };
+int ledCount = sizeof(ledPins);
+ 
+void setup() {
+
+    /* Initialize serial communications */
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
+    /* Initialize serial communications */
+    Serial.begin(BAUDRATE);
+
+    /* Initialize LED pins */
+    for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+        pinMode(ledPins[thisLed], OUTPUT);
+        digitalWrite(ledPins[thisLed], LOW);
+    }
+}
+ 
+void loop() {  
+    range(20, 50);
+    delay(100);
+} 
+ 
+int getDistance() {
+    long pulseTime, distance;
+
+    /* sensor procedure */
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    /* getting the pulse */
+    pulseTime = pulseIn(ECHO_PIN, HIGH);
+
+    /* calculating the distance */
+    distance = pulseTime / 58;
+
+    return distance;
+}
+
+void range(int min_range, int max_range) {
+    int range = getDistance();
+    
+    lightUpLed(range);
+    if( (range > min_range) && (range < max_range)) {
+        digitalWrite(BUZZER_PIN, HIGH);
+    }
+    else{
+        digitalWrite(BUZZER_PIN, LOW);
+    }
+}
+
+void lightUpLed(int range){
+    int ledLevel = map(range, 0, 50, 0, ledCount);
+    
+    // loop over the LED array:
+    for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+        // if the array element's index is less than ledLevel,
+        // turn the pin for this element on:
+        if (thisLed < ledLevel) {
+            digitalWrite(ledPins[thisLed], LOW);
+        }
+        // turn off all pins higher than the ledLevel:
+        else {
+            digitalWrite(ledPins[thisLed], HIGH);
+        }
+    }
+}
